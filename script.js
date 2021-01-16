2bb5a45965eb8e66df70c8386298a3e594ecd57a
ProCoderVJ <vc892782@gmail.com>
2021-01-16 20:33:00 +0530

    done

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const fullName=document.getElementById("fullname");
-var email=document.getElementById("email");
+function validateForm(){
+    var name=document.getElementById("fullname").value;
+    var email=document.getElementById("email").value;
+    var submit=document.getElementById("btn").value;
+    if(name =="" && email ==""){
+        alert("Name and email should be filled out")
+    }else{
+document.write(submit);
+    }
+
+
+  
+    
+
+}
